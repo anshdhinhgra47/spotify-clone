9482c170f08f38e133224396ab46564efb482bf8
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-10-08 00:22:06 +0530

    Set up Data Layers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from './Player';
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
---
+++ src/reducer.js
@@ -0,0 +1,23 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null
+}
+
+const reducer = (state, action) => {
+console.log(action);
+
+    switch(action.type) {
+        case 'SET_USER': 
+            return {
+                ...state,
+                user: action.user
+            }
+        default:
+            return state;
+    }
+}
+
+
+export default reducer;
