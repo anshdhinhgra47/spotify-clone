2bf42ddedc69fab5aba3cc46e01382e92c3fc97f
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-10-10 10:37:29 +0530

    Finished the Login

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import SpotifyWebApi from "spotify-web-api-js";
 import Player from './Player';
 import { useDataLayerValue } from "./DataLayer";
 
+
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
-  const [{}, dispatch] = useDataLayerValue();
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -18,17 +18,25 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      })
+
 
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
-        console.log('😁', user);
-      })
+    
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
     }
-
-    console.log('This is the token: ', token);
   }, []);
+
 
 
   return (
--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,9 @@ export const initialState = {
     user: null,
     playlists: [],
     playing: false,
-    item: null
+    item: null,
+    // token value to be removed (have it null)
+    token: 'BQCYqZGHMEbCEmtVVF1Cf2QRK69dEibAv2FL0VHDpcMLhFTFd2_k6xYBoKWIjSvZhpgtRR82xGVpqcejmjkyg91NGe7fNcg-mzT-KD3gMmktcBgQTHAB0Bwfkylgob84y-apjk_HTanqzQrTqQX-SK0Q8R9sapMZH1NzJy-xQqhtk9ULp0C1'
 }
 
 const reducer = (state, action) => {
@@ -13,11 +15,17 @@ console.log(action);
             return {
                 ...state,
                 user: action.user
+            };
+
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
             }
         default:
             return state;
     }
-}
+};
 
 
 export default reducer;
