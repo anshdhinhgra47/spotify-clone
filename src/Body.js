c0ae0343380ba25291bf6c5ccfe967bacd086538
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2020-10-12 00:50:55 +0530

    Completed upper body

--- src/Body.js
+++ src/Body.js
@@ -7,6 +7,15 @@ function Body({ spotify }) {
     return (
         <div className="body">
            <Header spotify={spotify} />
+
+           <div className="body__info">
+                <img src="https://newjams-images.scdn.co/v2/discover-weekly/aAbca4VNfzWuUCQ_FGiEFA==/bmVuZW5lbmVuZW5lbmVuZQ==/default" alt="" />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>Description.....</p>
+                </div>
+           </div>
         </div>
     )
 }
